64958d729b7fa92dae97d38e00155bca5451034c
Claude LE BRIS <claude.lebris@gmail.com>
2017-02-27 17:36:54 +0100

    Add Webpack (#11)
    * feat: add webpack support

---
+++ src/Webpack/README.md
@@ -0,0 +1,51 @@
+# Webpack provider for Onyx
+
+Must be declared after Twig.
+
+```
+use Onyx\Providers;
+use Onyx\Webpack\WebpackServiceProvider;
+
+// [...]
+
+$this->register(new Providers\Twig());
+$this->register(new WebpackServiceProvider());
+```
+
+Example in Twig views :
+
+```
+<html>
+    <head>
+
+    <!-- [...] -->
+
+    {% block stylesheet %}
+        {% for asset in ['vendor.scss', 'main.scss', 'css.css'] %}
+            <link rel="stylesheet" type="text/css" href="{{ webpackAsset(asset) }}">
+        {% endfor %}
+    {% endblock %}
+
+    {% if webpackChunkManifest is not empty %}
+        <script>
+        //<![CDATA[
+        window.webpackManifest = {{ webpackChunkManifest|raw }}
+        //]]>
+        </script>
+    {% endif %}
+    </head>
+
+    <body>
+
+    <!-- [...] -->
+
+    {% block javascript %}
+        {% for asset in ['common.js', 'vendor.main.js', 'main.js'] %}
+        <script src="{{ webpackAsset(asset) }}"></script>
+        {% endfor %}
+    {% endblock %}
+    </body>
+
+</html>
+
+```
---
+++ src/Webpack/WebpackServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Onyx\Webpack;
+
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+use Onyx\Webpack\Manifest;
+use Onyx\Traits;
+
+class WebpackServiceProvider implements ServiceProviderInterface
+{
+    use
+        Traits\PathManipulation;
+
+    public function register(Container $container)
+    {
+        $container['webpack.manifest.path'] = $this->enforceEndingSlash($container['root.path']) . 'www/assets/webpack-manifest.json';
+        $container['webpack.chunk.manifest.path'] = $this->enforceEndingSlash($container['root.path']) . '/www/assets/chunk-manifest.json';
+
+        $container['webpack.manifest'] = function($c) {
+            return new Manifest($c['webpack.manifest.path'], $c['webpack.chunk.manifest.path']);
+        };
+
+        if($container->offsetExists('twig'))
+        {
+            $container->extend('twig', function($twig) use ($container) {
+                $twig->addExtension(new TwigExtension($container['webpack.manifest']));
+
+                return $twig;
+            });
+        }
+    }
+}
