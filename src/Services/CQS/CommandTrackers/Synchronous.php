cfdea0f602abe7fd3b6161b9875629356fe0ec40
Julian Prud'homme <jprudhomme@naoned.fr>
2017-05-31 14:53:49 +0200

    feat: add a command tracker to allow command result retrieval

---
+++ src/Services/CQS/CommandTrackers/Synchronous.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Onyx\Services\CQS\CommandTrackers;
+
+use Onyx\Services\CQS\CommandTracker;
+
+class Synchronous implements CommandTracker
+{
+    private
+        $trackings;
+
+    public function __construct()
+    {
+        $this->trackings = [];
+    }
+
+    public function track(string $trackingId, $data): void
+    {
+        if(array_key_exists($trackingId, $this->trackings))
+        {
+            throw new \LogicException(sprintf('Tracking id "%s" already exists in command tracker', $trackingId));
+        }
+
+        $this->trackings[$trackingId] = $data;
+    }
+
+    public function retrieveTrackedData(string $trackingId)
+    {
+        if(! array_key_exists($trackingId, $this->trackings))
+        {
+            throw new \Exception(sprintf('Tracking id "%s" not found in command tracker', $trackingId));
+        }
+
+        return $this->trackings[$trackingId];
+    }
+}
