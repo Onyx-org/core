65e73d12c436685a0fe7dddfa526bbd69ff31b31
Julian Prud'homme <jprudhomme@naoned.fr>
2017-05-31 15:39:41 +0200

    fix: rename trackable classes

--- src/Services/Trackable.php
+++ src/Services/Trackable.php
@@ -4,20 +4,7 @@ declare(strict_types = 1);
 
 namespace Onyx\Services;
 
-use Ramsey\Uuid\Uuid;
-
-abstract class Trackable implements TrackableObject
+interface Trackable
 {
-    private
-        $trackingId;
-
-    public function __construct()
-    {
-        $this->trackingId = (Uuid::uuid4())->toString();
-    }
-
-    public function getTrackingId()
-    {
-        return $this->trackingId;
-    }
+    public function getTrackingId(): string;
 }
