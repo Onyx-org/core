baf3f27d08c5783b83e0f6446282a8811d9c33aa
Niktux <nico.ln@gmail.com>
2017-06-21 15:30:21 +0200

    feat(wizards): add cqs command wizard

---
+++ wizards/command/__ONYX_CommandName/__ONYX_CommandNameCommand.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace __ONYX_Namespace\Domain\Commands\__ONYX_CommandName;
+
+use Onyx\Services\CQS\Command;
+
+final class __ONYX_CommandNameCommand implements Command
+{
+    public
+        $oneFieldYouHaveToRenameIfYouDontWantToBeShamefulWhileTeammatesReviewYourPullRequest;
+}
