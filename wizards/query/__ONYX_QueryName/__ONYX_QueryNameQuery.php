10bc3ba05c5516037447722a7ed2dc294a9a4550
Niktux <nico.ln@gmail.com>
2017-06-21 15:22:20 +0200

    feat(wizards): add cqs query wizard

---
+++ wizards/query/__ONYX_QueryName/__ONYX_QueryNameQuery.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace __ONYX_Namespace\Domain\Queries\__ONYX_QueryName;
+
+use Onyx\Services\CQS\Query;
+
+final class __ONYX_QueryNameQuery implements Query
+{
+    public
+        $oneFieldYouHaveToRenameIfYouDontWantToBeShamefulWhileTeammatesReviewYourPullRequest;
+}
