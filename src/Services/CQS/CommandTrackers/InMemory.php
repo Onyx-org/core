96ef8b5282c0faa4b0d2e5965e41902bad721e72
Charron Maxime <mcharron@naoned.fr>
2017-08-23 18:43:35 +0200

    feat(cqs): inMemory command tracker (#30)
    * feat: inmemory command tracker
    
    * fix: PR review

---
+++ src/Services/CQS/CommandTrackers/InMemory.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Onyx\Services\CQS\CommandTrackers;
+
+use Onyx\Services\CQS\CommandTracker;
+
+class InMemory implements CommandTracker
+{
+    private
+        $data;
+
+    public function __construct(array $data = [])
+    {
+        $this->data = $data;
+    }
+
+    public function track(string $trackingId, $data): void
+    {
+        $this->data[$trackingId] = $data;
+    }
+
+    public function retrieveTrackedData(string $trackingId)
+    {
+        if (! array_key_exists($trackingId, $this->data))
+        {
+            throw new \RuntimeException(sprintf('Tracking id "%s" not found in command tracker', $trackingId));
+        }
+
+        return $this->data[$trackingId];
+    }
+}
---
+++ tests/Services/CQS/CommandTrackers/InMemoryTest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Onyx\Services\CQS\CommandTrackers;
+
+use PHPUnit\Framework\TestCase;
+
+class InMemoryTest extends TestCase
+{
+    private
+        $tracker,
+        $data;
+
+    protected function setUp()
+    {
+        $this->data = [
+            'speed' => 'rene',
+            'rene' => 'la taupe',
+        ];
+        $this->tracker = new InMemory($this->data);
+    }
+
+    public function testRetrieveData()
+    {
+        $retrievedData = $this->tracker->retrieveTrackedData('speed');
+        $expectedData = $this->data['speed'];
+
+        $this->assertEquals($expectedData, $retrievedData);
+    }
+
+    public function testTrackAlreadyTrackedData()
+    {
+        $this->assertEquals('la taupe', $this->tracker->retrieveTrackedData('rene'));
+
+        $this->tracker->track('rene', 'taupe');
+
+        $this->assertEquals('taupe', $this->tracker->retrieveTrackedData('rene'));
+    }
+
+    /**
+     * @expectedException \RuntimeException
+     */
+    public function testRetrieveUnknownData()
+    {
+        $this->tracker->retrieveTrackedData('unknown-data');
+    }
+}
